3001ba7091eec700912d487cda4f13fff8c60801
dni ⚡ <office@dnilabs.com>
2024-09-24 07:31:46 +0200

    feat: update to lnbits 1.0.0 (#2)

--- crud.py
+++ crud.py
@@ -19,86 +19,76 @@ async def create_faucet(data: CreateFaucet) -> Faucet:
     faucet = Faucet(id=faucet_id, next_tick=data.start_time, **data.dict())
     await db.execute(
         insert_query("faucet.faucet", faucet),
-        (*faucet.dict().values(),),
+        faucet.dict(),
     )
 
     return faucet
 
 
 async def delete_faucet(faucet_id: str) -> None:
-    await db.execute("DELETE FROM faucet.faucet WHERE id = ?", (faucet_id,))
-    await db.execute("DELETE FROM faucet.secret WHERE faucet_id = ?", (faucet_id,))
+    await db.execute("DELETE FROM faucet.faucet WHERE id = :id", {"id": faucet_id})
+    await db.execute(
+        "DELETE FROM faucet.secret WHERE faucet_id = :id", {"id": faucet_id}
+    )
 
 
 async def get_faucet(faucet_id: str) -> Optional[Faucet]:
-    row = await db.fetchone("SELECT * FROM faucet.faucet WHERE id = ?", (faucet_id,))
+    row = await db.fetchone(
+        "SELECT * FROM faucet.faucet WHERE id = :id", {"id": faucet_id}
+    )
     return Faucet(**row) if row else None
 
 
 async def get_active_faucets() -> list[Faucet]:
     now = int(datetime.datetime.now().timestamp())
-    ph = db.timestamp_placeholder
+    ph = db.timestamp_placeholder("now")
     rows = await db.fetchall(
         f"""
         SELECT * FROM faucet.faucet WHERE start_time <= {ph} AND end_time >= {ph}
         """,
-        (
-            now,
-            now,
-        ),
+        {"now": now},
     )
     return [Faucet(**row) for row in rows]
 
 
 async def get_faucets(wallet_ids: list[str]) -> list[Faucet]:
-    rows = await db.fetchall(
-        "SELECT * FROM faucet.faucet WHERE wallet IN ({})".format(
-            ",".join("?" * len(wallet_ids))
-        ),
-        (*wallet_ids,),
-    )
+    q = ",".join([f"'{w}'" for w in wallet_ids])
+    rows = await db.fetchall(f"SELECT * FROM faucet.faucet WHERE wallet IN ({q})")
     return [Faucet(**row) for row in rows]
 
 
 async def update_faucet(faucet: Faucet) -> Faucet:
-    await db.execute(
-        update_query("faucet.faucet", faucet),
-        (*faucet.dict().values(), faucet.id),
-    )
+    await db.execute(update_query("faucet.faucet", faucet), faucet.dict())
     return faucet
 
 
 async def create_faucet_secret(secret: FaucetSecret) -> FaucetSecret:
-    await db.execute(
-        insert_query("faucet.secret", secret),
-        (*secret.dict().values(),),
-    )
+    await db.execute(insert_query("faucet.secret", secret), secret.dict())
     return secret
 
 
 async def get_faucet_secret(k1: str) -> Optional[FaucetSecret]:
-    row = await db.fetchone("SELECT * FROM faucet.secret WHERE k1 = ?", (k1,))
+    row = await db.fetchone("SELECT * FROM faucet.secret WHERE k1 = :k1", {"k1": k1})
     return FaucetSecret(**row) if row else None
 
 
 async def get_next_faucet_secret(faucet_id: str) -> Optional[FaucetSecret]:
     row = await db.fetchone(
         """
-        SELECT * FROM faucet.secret
-        WHERE used_time IS NULL AND faucet_id = ?
+        SELECT * FROM faucet.secret WHERE used_time IS NULL AND faucet_id = :id
         """,
-        (faucet_id,),
+        {"id": faucet_id},
     )
     return FaucetSecret(**row) if row else None
 
 
 async def delete_faucet_secret(k1: str) -> None:
-    await db.execute("DELETE FROM faucet.secret WHERE k1 = ?", (k1,))
+    await db.execute("DELETE FROM faucet.secret WHERE k1 = :k1", {"k1": k1})
 
 
 async def update_faucet_secret(secret: FaucetSecret) -> FaucetSecret:
     await db.execute(
-        update_query("faucet.secret", secret, "WHERE k1 = ?"),
-        (*secret.dict().values(), secret.k1),
+        update_query("faucet.secret", secret, "WHERE k1 = :k1"),
+        secret.dict(),
     )
     return secret
